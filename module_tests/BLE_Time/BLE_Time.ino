3d2164d0fba8d2c1543ae08d4af269d69e9ea5b6
Cycrus <marxcyril@gmail.com>
2024-07-16 11:58:17 +0200

    Tested out oled display.

--- module_tests/BLE_Time/BLE_Time.ino
+++ module_tests/BLE_Time/BLE_Time.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ArduinoBLE.h>
 
+BLEService timeService("1805"); // UUID for Current Time Service
+BLEUnsignedLongCharacteristic currentTimeChar("2A2B", BLEWrite); // UUID for Current Time Characteristic
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!BLE.begin()) {
+    Serial.println("Starting BLE failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("Nano33BLE_Time");
+  BLE.setAdvertisedService(timeService);
+  timeService.addCharacteristic(currentTimeChar);
+  BLE.addService(timeService);
+
+  currentTimeChar.writeValue(0); // Initialize characteristic with default value
+
+  BLE.advertise();
+  Serial.println("Bluetooth device active, waiting for connections...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  BLEDevice central = BLE.central();
 
+  if (central) {
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+
+    while (central.connected()) {
+      if (currentTimeChar.written()) {
+        unsigned long currentTime = currentTimeChar.value();
+        Serial.print("Received time: ");
+        Serial.println(currentTime);
+      }
+    }
+
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
 }
